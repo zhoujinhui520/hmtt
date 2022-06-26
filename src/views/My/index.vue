eba1363ce81c3e02dd96f4c5dc6053a7afbe4dd7
zhoujinhui520 <1130358720@qq.com>
2022-06-26 11:45:23 +0800

    路由配置完成

--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-      <!-- 一级占位符 -->
-    <router-view></router-view>
-  </div>
+<div>
+    我的
+</div>
 </template>
 
 <script>
 export default {
-  created () { },
+  created () {},
   data () {
     return {}
   },
@@ -19,5 +18,6 @@ export default {
 }
 </script>
 
-<style scoped lang='less'>
+<style scoped>
+
 </style>
